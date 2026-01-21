37c427b23eafa804546e0ad2451d436b44c26d19
Einav Haimovich <einav@haimol.net>
2026-01-21 21:41:54 +0200

    basics

--- Async Programming In C#/LifeBeforeAsync/Program.cs
+++ Async Programming In C#/LifeBeforeAsync/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LifeBeforeAsync;
+
+Console.WriteLine("Cooking has started!");
+var turkey = new Turkey();
+var gravy = new Gravy();
+
+
+await Task.WhenAll(turkey.Cook(), gravy.Cook());
+
+Console.WriteLine("Cooking is complete!");
